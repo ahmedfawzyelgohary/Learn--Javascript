1ddea6d2cf9473726cf23dfc18f08137cd0b0877
elgohary <ahmedfawyelgohary@yahoo.com>
2023-06-03 21:11:11 +0300

    learn data type and slove the assignment

--- 01-Fundamentals-Part-1/assignments.js
+++ 01-Fundamentals-Part-1/assignments.js
@@ -1,7 +1,15 @@
-// Values and Variables
+// (Values and Variables)
 let country = "Egypty";
 let continent = "Africa";
 let population = "100";
 console.log(country);
 console.log(continent);
 console.log(population);
+
+//(Data Type)
+let isIsland = false;
+let language;
+console.log(typeof isIsland);
+console.log(typeof language);
+console.log(typeof country);
+console.log(typeof population);
