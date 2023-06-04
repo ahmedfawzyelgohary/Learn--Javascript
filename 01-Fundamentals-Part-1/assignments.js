38dfb7964b6e73f7a7fe2c612ae82fdaba803b18
elgohary <ahmedfawyelgohary@yahoo.com>
2023-06-04 15:26:47 +0300

    learn about let,const and var

--- 01-Fundamentals-Part-1/assignments.js
+++ 01-Fundamentals-Part-1/assignments.js
@@ -1,15 +1,23 @@
 // (Values and Variables)
-let country = "Egypty";
-let continent = "Africa";
-let population = "100";
-console.log(country);
-console.log(continent);
-console.log(population);
+// let country = "Egypty";
+// let continent = "Africa";
+// let population = "100";
+// console.log(country);
+// console.log(continent);
+// console.log(population);
 
 //(Data Type)
-let isIsland = false;
-let language;
-console.log(typeof isIsland);
-console.log(typeof language);
-console.log(typeof country);
-console.log(typeof population);
+// let isIsland = false;
+// let language;
+// console.log(typeof isIsland);
+// console.log(typeof language);
+// console.log(typeof country);
+// console.log(typeof population);
+
+//let,const and var
+language = "Arabic";
+const country = "Egypt";
+const continent = "Africa";
+const isIsland = false;
+// try to change one of them
+country = "Tunis";
