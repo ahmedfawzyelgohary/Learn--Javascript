d5476a17795d89b0a63cf5822b312df6aa26ca3f
elgohary <ahmedfawyelgohary@yahoo.com>
2023-06-08 00:05:23 +0300

    learn Basic Operatoras

--- 01-Fundamentals-Part-1/assignments.js
+++ 01-Fundamentals-Part-1/assignments.js
@@ -1,7 +1,7 @@
 // (Values and Variables)
 // let country = "Egypty";
 // let continent = "Africa";
-// let population = "100";
+let population = 100;
 // console.log(country);
 // console.log(continent);
 // console.log(population);
@@ -20,4 +20,15 @@ const country = "Egypt";
 const continent = "Africa";
 const isIsland = false;
 // try to change one of them
-country = "Tunis";
+// country = "Tunis";
+
+// Basic Operators
+console.log(population / 2);
+population++;
+console.log(population);
+let finland = 6;
+console.log(population > finland);
+console.log(population < finland);
+const description =
+  "Portugal is in Europe,its about 11 million people speak protuguese";
+console.log(description);
