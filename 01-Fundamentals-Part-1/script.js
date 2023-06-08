035eca925c00750f7714f63bc876769c016c53c2
elgohary <ahmedfawyelgohary@yahoo.com>
2023-06-08 09:46:46 +0300

    solve challenge #1

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -1,4 +1,14 @@
 let js = "amazing";
 if (js === "amazing") alert("java script is fun!");
-console.log(12 + 12);
-//
+// console.log(12 + 12);
+
+//Coding Challenge #1
+const markMass = 78;
+const markHeight = 1.69;
+const johnHeight = 1.95;
+const johnMass = 95;
+const markBMI = markMass / markHeight ** 2;
+const johnBMI = johnMass / johnHeight ** 2;
+const markHeigherBMI = markBMI > johnBMI;
+console.log(markBMI, johnBMI);
+console.log(markHeigherBMI);
