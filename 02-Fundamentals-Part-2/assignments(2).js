ba1f333224143b9d8fe6a0f53696e1b25ad00878
elgohary <ahmedfawyelgohary@yahoo.com>
2023-06-14 23:40:10 +0300

    complet work

--- 02-Fundamentals-Part-2/assignments(2).js
+++ 02-Fundamentals-Part-2/assignments(2).js
@@ -18,9 +18,9 @@ const percentageWorldFinlanda = percentageOfWorld1(6);
 const percentageWorldEgypt = percentageOfWorld1(100);
 const percentageWorldItaly = percentageOfWorld1(59);
 
-console.log(
-  `${percentageWorldEgypt} % , ${percentageWorldFinlanda} % , ${percentageWorldItaly} %`
-);
+// console.log(
+//   `${percentageWorldEgypt} % , ${percentageWorldFinlanda} % , ${percentageWorldItaly} %`
+// );
 
 //Function Expression
 const percentageOfWorld2 = function (population) {
@@ -29,9 +29,9 @@ const percentageOfWorld2 = function (population) {
 const percentageWorldFinlanda1 = percentageOfWorld1(6);
 const percentageWorldEgypt1 = percentageOfWorld1(100);
 const percentageWorldItaly1 = percentageOfWorld1(59);
-console.log(
-  `${percentageWorldEgypt1} % , ${percentageWorldFinlanda1} % , ${percentageWorldItaly1} %`
-);
+// console.log(
+//   `${percentageWorldEgypt1} % , ${percentageWorldFinlanda1} % , ${percentageWorldItaly1} %`
+// );
 
 // ***********(Arrow Functions)***********
 const percentageOfWorld3 = (population) => (population / 7900) * 100;
@@ -39,7 +39,6 @@ const percentageOfWorld3 = (population) => (population / 7900) * 100;
 const percentageOfWorldFinlanda2 = percentageOfWorld3(6);
 const percentageOfWorldEgypt2 = percentageOfWorld3(100);
 const percentageOfWorldItaly2 = percentageOfWorld3(59);
-console.log(
-  `${percentageOfWorldEgypt2} % , ${percentageOfWorldFinlanda2} % , ${percentageOfWorldItaly2} %`
-);
-//
+// console.log(
+//   `${percentageOfWorldEgypt2} % , ${percentageOfWorldFinlanda2} % , ${percentageOfWorldItaly2} %`
+// );
