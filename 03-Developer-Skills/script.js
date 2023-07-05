5f21cd98d889f1157ee7eb388e2cbb211877cd77
elgohary <ahmedfawyelgohary@yahoo.com>
2023-07-05 16:25:20 +0300

    learn some new skills

--- 03-Developer-Skills/script.js
+++ 03-Developer-Skills/script.js
@@ -1,2 +1,13 @@
 // Remember, we're gonna use strict mode in all scripts now!
 'use strict';
+// Coding challenge #1
+const data1 = [17, 21, 23];
+const data2 = [12, 5, -5, 0, 4];
+const printForecast = function (arr) {
+  let str = '';
+  for (let i = 0; i < arr.length; i++) {
+    str += `${arr[i]}ºC in ${i + 1} days ... `;
+  }
+  console.log(`...` + str);
+};
+printForecast(data1);
